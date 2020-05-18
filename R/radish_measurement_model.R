123025c4bd2e178a55a5ebce7492ae73cd44f0d9
nspope <nspope@utexas.edu>
2020-05-18 15:57:56 -0700

    more docs

---
+++ R/radish_measurement_model.R
@@ -0,0 +1,8 @@
+#' Measurement models
+#'
+#' Functions that fit a matrix observed genetic distances to a submatrix of the inverse graph Laplacian
+#'
+#' @name radish_measurement_model
+#' @seealso \code{\link{leastsquares}}, \code{\link{mlpe}}, \code{\link{generalized_wishart}}
+#' @export
+NULL
